eca221097fc662c82596baecd18d238ae1f8eaa4
aarhil90 <aarhil90@kingshigh.school.nz>
2025-07-17 15:00:22 +1200

    redesigning  character

--- new-game-project/player.gd
+++ new-game-project/player.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
-
 @export var SPEED = 150.0
 @export var JUMP_VELOCITY = -300.0
 @export var BASE_FRICTION = 10
 @export var ICE_FRICTION = 5
 var active_friction = BASE_FRICTION
-@onready var ray : RayCast2D = $floorRayCast2D
+@onready var ray: RayCast2D = $floorRayCast2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready():
 	add_to_group("player")
@@ -21,10 +21,14 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		# Flip sprite based on direction
+		if direction > 0:  # Moving right
+			animated_sprite.flip_h = false
+		elif direction < 0:  # Moving left
+			animated_sprite.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, active_friction)
 
@@ -33,9 +37,7 @@ func _physics_process(delta: float) -> void:
 	if ray.is_colliding():
 		var collider = ray.get_collider()
 		if collider.is_in_group("FLOOR"):
-			
 			if collider.is_ice:
 				active_friction = ICE_FRICTION
 			else:
 				active_friction = BASE_FRICTION
-			

9f8a389f912296efdc943dcb8dbdcab5531f9161
aarhil90 <aarhil90@kingshigh.school.nz>
2025-07-23 12:28:02 +1200

    added spikes to game some what working

--- new-game-project/Scripts/killzone.gd
+++ new-game-project/Scripts/killzone.gd
@@ -1,10 +1,16 @@
 extends Area2D
 
 @onready var timer = $Timer
+@onready var sprite = $AnimatedSprite2D
+@onready var collision = $CollisionShape2D
+
+
 
 func _on_body_entered(body):
-	print(body.name)
-	timer.start()
+	if body.name == "Player":
+		print("ouch")
+		await get_tree().create_timer(1)
+		get_tree().reload_current_scene()
 
-func _on_timer_timeout():
-	get_tree().reload_current_scene()
+
+	

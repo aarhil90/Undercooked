edfb77800b7c4b2571b68f0bb6c9f619547d8e5f
aarhil90 <aarhil90@kingshigh.school.nz>
2025-07-30 11:09:47 +1200

    fixed menu just finish options and tutorial now

--- new-game-project/Scripts/menu.gd
+++ new-game-project/Scripts/menu.gd
@@ -1,18 +1,19 @@
 extends Control
 
 func _on_play_pressed() -> void:
-	var error = get_tree().change_scene_to_file("res://game.tscn")
-	if error != OK:
-		print("Failed to change scene: ", error)
-
-func _on_options_pressed() -> void:
-	pass # Replace with function body.
+	print ("play pressed")
+	get_tree().change_scene_to_file("res://game.tscn")
 
 
 func _on_tutorial_pressed() -> void:
-	var error = get_tree().change_scene_to_file("res://tutorial.tscn")
-	if error != OK:
-		print("Failed to change scene: ", error)
+	print ("tutorial pressed")
+	get_tree().change_scene_to_file("res://tutorial.tscn")
+
+
+func _on_options_pressed() -> void:
+	print ("options pressed")
+
 
 func _on_exit_pressed() -> void:
-	get_tree().quit() # Use quit() instead of exit()
+	print ("exit pressed")
+	get_tree().quit()

bfba3279ebe9d9e60d1227b8f21cf5a7ea0d1953
aarhil90 <aarhil90@kingshigh.school.nz>
2025-08-04 15:01:16 +1200

    changed music fixed tutorial added text started back button

--- new-game-project/Scripts/menu.gd
+++ new-game-project/Scripts/menu.gd
@@ -18,3 +18,7 @@ func _on_options_pressed() -> void:
 func _on_exit_pressed() -> void:
 	print ("exit pressed")
 	get_tree().quit()
+
+func _on_back_pressed() -> void:
+	print ("back pressed")
+	get_tree().change_scene_to_file("res://menu.tscn")
